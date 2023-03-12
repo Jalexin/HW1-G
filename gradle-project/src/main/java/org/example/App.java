78b793bc988a068a41da92bb3f86b7466d5f608e
Jalexin <111777487+Jalexin@users.noreply.github.com>
2023-03-12 18:32:00 +0200

    Add files via upload

---
+++ gradle-project/src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import com.google.gson.Gson;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        String name = "John";
+        String lastName = "Doe";
+
+        Person person = new Person(name, lastName);
+
+        Gson gson = new Gson();
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+    }
+}
